d79ede659596ff29ed646885c48d15551a95bb00
Kihyun Hwang <kihyun.hwang@m2utech.com>
2017-07-28 13:47:39 +0900

    file to file (o) file read and write (ing) file to db (todo) db to file (todo) . .

--- El-Camel/src/main/java/m2u/el/camel/HomeController.java
+++ El-Camel/src/main/java/m2u/el/camel/HomeController.java
@@ -8,14 +8,16 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.http.HttpEndpoint;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import m2u.el.camel.domain.LineProcessor;
 
 /**
  * Handles requests for the application home page.
@@ -35,41 +37,65 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public void home(Locale locale, Model model) throws Exception {
 		logger.info("Welcome home! The client locale is {}.", locale);
-
-		// TODO : delete.....testing....
-		// if ( flag.get() ) {
-		//
-		// test2();
-		//
-		// flag.set(false);
-		// }
 	}
 
-	public static void main(String[] args) {
-		test2();
+	public static void main(String[] args) throws Exception {
+//		fileCopyTest();
+		fileModifyCopyTest();
+//		fileToDBTest();
 	}
 
-	private static void test1() throws Exception {
+	private static void fileToDBTest() {
 
-		logger.info("Camel Test 1 Started ...");
+		logger.info("File to DB test started ...");
 
 		CamelContext context = new DefaultCamelContext();
 
-		context.addRoutes(new RouteBuilder() {
-			public void configure() {
-				from("file:/d:/kihyun/z99. temp/camel/from").to("file:/d:/kihyun/z99. temp/camel/to");
-			}
-		});
-		context.start(); // start하면 계속 watch하고 있다가 파일이 추가될 경우 자동으로 move함.
-		Thread.sleep(20000);
-		context.stop();
+		try {
+			context.addRoutes(new RouteBuilder() {
+				public void configure() {
+					// from("servlet:localhost:8080/aibot")
+					from("file:/d:/kihyun/z99. temp/camel/from/*.csv")
+					.process(new Processor() {
+						@Override
+						public void process(Exchange arg0) throws Exception {
+							System.out.println("processing file to db......");
+						}
+					})
+					.to("jdbc:oracle:thin:localhost:1521/xe");
+				}
+			});
+			context.setHandleFault(false);
+			context.start();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 
-		logger.info("Camel Test 1 Stopped ...");
+//		ConsumerTemplate ct = context.createConsumerTemplate();
+//		Exchange in = ct.receive("http://localhost:8080/aibot");
+
+		ProducerTemplate pt = context.createProducerTemplate();
+		
+		Exchange out = pt.request(
+				"http:localhost:9080/ProgramD/GetBotResponse?input=로또",
+				new Processor() {
+					@Override
+					public void process(Exchange arg0) throws Exception {
+						System.out.println("processing message......2222");
+					}
+				});
+
+		String resp = out.getOut().getBody(String.class);
+		System.out.println("resp : " + resp);
+
+		// Thread.currentThread().join();
+		// logger.info("thread joined");
 	}
-
+	
 	private static void test2() {
 
-		logger.info("Camel Test 2 Started ...");
+		logger.info("File to DB test started ...");
 
 		CamelContext context = new DefaultCamelContext();
 
@@ -111,8 +137,7 @@ public class HomeController {
 
 		ProducerTemplate pt = context.createProducerTemplate();
 		
-		Exchange out = pt.request(
-				"http:localhost:9080/ProgramD/GetBotResponse?input=로또",
+		Exchange out = pt.request("http:localhost:9080/ProgramD/GetBotResponse?input=로또",
 				new Processor() {
 					@Override
 					public void process(Exchange arg0) throws Exception {
@@ -133,5 +158,45 @@ public class HomeController {
 
 		// Thread.currentThread().join();
 		// logger.info("thread joined");
+	}
+	
+	private static void fileCopyTest() throws Exception {
+
+		logger.info("Copying file test started !!!");
+
+		CamelContext context = new DefaultCamelContext();
+
+		context.addRoutes(new RouteBuilder() {
+			public void configure() {
+				from("file:/d:/kihyun/z99. temp/camel/from").to("file:/d:/kihyun/z99. temp/camel/to");
+			}
+		});
+		context.start(); // start하면 계속 watch하고 있다가 파일이 추가될 경우 자동으로 move함.
+//		Thread.sleep(10000);
+//		context.stop();
+
+		logger.info("Copying file test finished !!!");
+	}
+
+	private static void fileModifyCopyTest() throws Exception {
+
+		logger.info("Modifying and copying file test started !!!");
+
+		final CamelContext context = new DefaultCamelContext();
+		final LineProcessor lineProcessor = new LineProcessor();
+		
+		context.addRoutes(new RouteBuilder() {
+			public void configure() {
+				from("file:/d:/kihyun/z99. temp/camel/from")
+				.split()
+				.tokenize(System.lineSeparator())
+				.process(lineProcessor)
+				.to("file:/d:/kihyun/z99. temp/camel/to");
+			}
+		});
+		context.start(); // start하면 계속 watch하고 있다가 파일이 추가될 경우 자동으로 move함.
+		Thread.currentThread().join();
+
+		logger.info("Modifying and copying file test finished !!!");
 	}
 }
--- El-Camel/src/main/java/m2u/el/camel/domain/LineProcessor.java
+++ El-Camel/src/main/java/m2u/el/camel/domain/LineProcessor.java
@@ -3,14 +3,18 @@ package m2u.el.camel.domain;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
 
-public class MyProcessor implements Processor{
+@Component
+public class LineProcessor implements Processor{
 
+	int lineNum;
+	
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		Message msg = exchange.getIn();
-		String body = (String)msg.getBody();
-		System.out.println("body : " + body);
+		String line = (String)msg.getBody(String.class);
+		System.out.println("line : " + line);
 	}
 
 }
